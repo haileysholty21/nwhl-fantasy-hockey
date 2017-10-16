45376498f44606b220adc355a26dc32f2c9eeb9f
Hailey Sholty <haileysholty21@aol.com>
2017-10-16 13:53:01 -0400

    Integrated new html and css files
    Updated my username for the commit

--- nwhl_fantasy/urls.py
+++ nwhl_fantasy/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
 from nwhl_fantasy import views
 
-urlpatterns = [url(r'^$', views.HomePageView.as_view()),]
+
+urlpatterns = [url(r'^$', views.HomePageView.as_view()),] 
